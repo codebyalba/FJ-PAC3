37efcbee91af2e0a6ff3152e83bc41ad42ccaa36
Alba Diaz Romero <adiazrom26@gmail.com>
2025-11-20 20:26:16 +0100

    configure app routes

--- PEC3_Ej_Prac/ecommerce/src/app/app.routes.ts
+++ PEC3_Ej_Prac/ecommerce/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ArticleList } from './components/article-list/article-list';
+import { ArticleNewTemplate } from './components/article-new-template/article-new-template';
+import { ArticleNewReactive } from './components/article-new-reactive/article-new-reactive';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'articles', pathMatch: 'full' },
+    { path: 'articles', component: ArticleList },
+    { path: 'new-template-article', component: ArticleNewTemplate },
+    { path: 'new-reactive-article', component: ArticleNewReactive },
+  ];
