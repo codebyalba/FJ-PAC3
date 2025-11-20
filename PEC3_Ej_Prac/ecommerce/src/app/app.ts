472e8c6892c7960f3a8da1d582de81b9d3d8c262
Alba Diaz Romero <adiazrom26@gmail.com>
2025-11-20 18:11:53 +0100

    display articles in app component

--- PEC3_Ej_Prac/ecommerce/src/app/app.ts
+++ PEC3_Ej_Prac/ecommerce/src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArticleItem } from './components/article-item/article-item';
+import { Article } from './models/article.model';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, ArticleItem, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
+
 export class App {
-  protected readonly title = signal('ecommerce');
+  title = 'Ecommerce';
+
+  articles = [
+    {
+      name: 'Reloj de lujo',
+      imageUrl: 'assets/img/elegant-watch-with-silver-golden-chain-isolated.jpg',
+      price: 129.99,
+      isOnSale: true,
+      quantityInCart: 0
+    },
+    {
+      name: 'Auriculares Bluetooth',
+      imageUrl: 'assets/img/headphones-displayed-against-dark-background.jpg',
+      price: 79.99,
+      isOnSale: false,
+      quantityInCart: 0
+    }
+  ];
+
+  trackByArticleName(index: number, article: Article) {
+    return article.name;
+  }
 }
